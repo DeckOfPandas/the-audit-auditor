2886e9e02b5c4e4ffd4d6f3fa0ae6d9c133c3907
swlim <seckwei93@gmail.com>
2018-06-30 17:09:11 +0100

    Fix main.go

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+    "os"
+    "strconv"
 	_ "auditaudit/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+    port, _ := strconv.Atoi(os.Getenv("PORT"))
+    beego.BConfig.Listen.HTTPPort = port
 	beego.Run()
 }
 
